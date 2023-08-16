931846b47600b1484b9a8dfdf722596a11472bb0
Miguel M <miguel.maldonado5802@gmail.com>
2023-08-16 14:11:38 -0400

    was having trouble with the updating button text and interaction. Got that figured out

--- js/script.js
+++ js/script.js
@@ -1,33 +1,60 @@
 var right = 0;
 var wrong = 0;
+var counter = 0;
 var rightEl = document.querySelector("#right");
 var wrongEl = document.querySelector("#wrong");
 var timmer = 0;
 var questions = document.querySelector(".quiz-questions");
-var temptext = document.querySelector(".temp-text");
+var introText = document.querySelector(".intro-text");
 var quiz = document.querySelector(".quiz-container");
-var quizBtn = document.querySelectorAll(".questions");
-var q1 = {
-    q1Question: "Question 1: How do you spell door?",
-    q1Answers: ["Floor", "Door", "dooooor", "Moore"],
-}
-var q2 = {
-    q2Question: "Question 2: what is the number 1?",
-    q2Answers: ["1", "2", "3", "4"],
+var quizBtns = document.querySelectorAll(".questions");
+var quizBtn = document.querySelector(".questions");
+var btn1El = document.querySelector(".question-1");
+var btn2El = document.querySelector(".question-2");
+var btn3El = document.querySelector(".question-3");
+var btn4El = document.querySelector(".question-4");
+var startG = document.querySelector(".start-game");
+var btnQuestion = document.querySelectorAll(".btn-question");
+
+// moved the individual queston arrays into one object that has the multiple arrays with answers.
+var questionAll = {
+    questions: ["Question 1: How do you spell door?", "Question 2: what is the number 1?", "Question 3: Which is a fruit"],
+    answers: [["Floor", "Door", "dooooor", "Moore"],["1", "2", "3", "4"],["Strawbery", "Coconut", "Tomatoe", "Dirt"],[],[]],
+    correct: ["Door", "1","Tomatoe"]
 }
 
 // updates the right and wrong text in the html
 function rightCounter() {
     rightEl.textContent = right;
 }
-
 function wrongCounter() {
     wrongEl.textContent = wrong;
 }
 
-quiz.addEventListener("click", function(event) {
-    // changes H1 to the question, clears out the text of the start menu, hides the .start-game button.
-    temptext.textContent = "";
+// these update the text element for the question buttons
+function btn1Question() {
+    btn1El.setAttribute("style", "display: block");
+    btn1El.textContent = question1
+}
+function btn2Question() {
+    btn2El.setAttribute("style", "display: block");
+    btn2El.textContent = question2
+}
+function btn3Question() {
+    btn3El.setAttribute("style", "display: block");
+    btn3El.textContent = question3
+}
+function btn4Question() {
+    btn4El.setAttribute("style", "display: block");
+    btn4El.textContent = question4
+}
+
+
+// first event listener to start-game button. When "take quiz" is clicked it will hide the intro text and start button. Then unhide the question buttons. 
+startG.addEventListener("click", function() {
+
+    // hides the introText and start-game button
+    introText.textContent = "";
     var startGameButton =  document.querySelector(".start-game");
     var hidStart = startGameButton.getAttribute("data-state");
     
@@ -35,86 +62,137 @@ quiz.addEventListener("click", function(event) {
         startGameButton.setAttribute("style", "display: none;");
         startGameButton.setAttribute("data-state", "hidden");
     }
-    // selects all the question buttons and change their display back to on
-    var questionButton = document.querySelectorAll(".questions");
-    questionButton.forEach(function(questionButton){
-        var questionStatus = questionButton.getAttribute("data-state");
+
+    // changes the h1 text
+    questions.textContent = questionAll.questions[counter]
     
-        if (questionStatus == "hidden") {
-            questionButton.setAttribute("style", "display: block")
-        }
-    })
+    // updates the question button text
+    question1 = questionAll.answers[counter][0];
+    btn1Question()
+
+    question2 = questionAll.answers[counter][1];
+    btn2Question()
+
+    question3 = questionAll.answers[counter][2];
+    btn3Question()
+
+    question4 = questionAll.answers[counter][3];
+    btn4Question()
 })
+
+// forEach loop to add the eventListener to each button, 
+// then update the text of each button with the next question
+btnQuestion.forEach(function(btn) {
+    if (count > 5 ) {
+        return;
+    }
+    btn.addEventListener("click", function(event){ 
+        answerCheck = event.target.textContent;
+        
+        if (answerCheck == questionAll.correct[counter]) {
+            right++;
+            rightCounter()
+        } else {
+            wrong++;
+            wrongCounter()
+        }
+    
+        counter++;
+        questions.textContent = questionAll.questions[counter];
+    
+        question1 = questionAll.answers[counter][0];
+        console.log(q2.q2Answers)
+        btn1Question()
+    
+        question2 = questionAll.answers[counter][1];
+        btn2Question()
+    
+        question3 = questionAll.answers[counter][2];
+        btn3Question()
+    
+        question4 = questionAll.answers[counter][3];
+        btn4Question()
+        
+    })
+    
+});
+
+
+
+
+
+
 
 
 
 // was having a issue with having the questions starting one afteranother. Asked ChatGPT and it came up with "promises and chaining"
-function question1(event) {
-    // event.preventDefault
-    // return new Promise((resolve) => {
-        questions.textContent = q1.q1Question;
-        // iterate throught the q array and inserts the question text in the button elements
-        for (var index = 0; index < questionButton.length; index++) {
-            questionButton[index].textContent = q1.q1Answers[index]
-        }
+// function question1(event) {
+//     // event.preventDefault
+//     // return new Promise((resolve) => {
+//         // var questionButton = document.querySelectorAll(".questions");
+//         // questions.textContent = q1.q1Question;
+//         // // iterate throught the q array and inserts the question text in the button elements
+//         // for (var index = 0; index < questionButton.length; index++) {
+//         //     questionButton[index].textContent = q1.q1Answers[index]
+//         // }
 
-        // selecting just the question buttons
-        var qbg = document.querySelector(".question-btn-group");
+//         // selecting just the question buttons
+//         var qbg = document.querySelector(".question-btn-group");
 
-        // event click to the question buttons to decied on the answer. 
-        function handler(event) {
-            const answerCheck = event.target.textContent;
+//         // event click to the question buttons to decied on the answer. 
+//         function handler(event) {
+//             const answerCheck = event.target.textContent;
         
-            if (answerCheck == "Door") {
-                right++;
-                rightCounter()
-            } else {
-                wrong++;
-                wrongCounter()
-            }
+//             if (answerCheck == "Door") {
+//                 right++;
+//                 rightCounter()
+//             } else {
+//                 wrong++;
+//                 wrongCounter()
+//             }
 
-            qbg.removeEventListener("click", handler);
-            resolve();
-        };
+//             qbg.removeEventListener("click", handler);
+//             resolve();
+//         };
 
-        qbg.addEventListener("click", handler);
-};
+//         qbg.addEventListener("click", handler);
+// };
 
 
-function question2() {
-    return new Promise((resolve) => {
-        questions.textContent = q2.q2Question;
-        // iterate throught the q array and inserts the question text in the button elements
-        for (var index = 0; index < questionButton.length; index++) {
-            questionButton[index].textContent = q2.q2Answers[index]
-        }
+// function question2() {
+//     return new Promise((resolve) => {
+//         questions.textContent = q2.q2Question;
+//         // iterate throught the q array and inserts the question text in the button elements
+//         for (var index = 0; index < questionButton.length; index++) {
+//             questionButton[index].textContent = q2.q2Answers[index]
+//         }
 
-        // selecting just the question buttons
-        var qbg = document.querySelector(".question-btn-group");
+//         // selecting just the question buttons
+//         var qbg = document.querySelector(".question-btn-group");
 
-        // event click to the question buttons to decied on the answer. 
-        function handler(event) {
-            const answerCheck = event.target.textContent;
+//         // event click to the question buttons to decied on the answer. 
+//         function handler(event) {
+//             const answerCheck = event.target.textContent;
         
-            if (answerCheck == "1") {
-                right++;
-                rightCounter()
-            } else {
-                wrong++;
-                wrongCounter()
-            }
+//             if (answerCheck == "1") {
+//                 right++;
+//                 rightCounter()
+//             } else {
+//                 wrong++;
+//                 wrongCounter()
+//             }
 
-            qbg.removeEventListener("click", handler);
+//             qbg.removeEventListener("click", handler);
 
-            resolve();
-        };
+//             resolve();
+//         };
 
-        qbg.addEventListener("click", handler);
-    });
-}
+//         qbg.addEventListener("click", handler);
+//     });
+// }
 
-question1()
-    .then(question2)
+// question1()
+//     .then(question2)
 
 
 
